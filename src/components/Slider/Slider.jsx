78ec1ad654586d277fcbf4453ffc423287fc2204
AnzhelikaMullova <ulanova592@gmail.com>
2023-08-21 12:21:22 +0300

    add commit test

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -2,45 +2,36 @@ import st from './slider.module.scss';
 import data from '../../data.json';
 import Cards from './Cards';
 import { useEffect, useState } from 'react';
-
 export default function Slider() {
-    const [cards, setCards] = useState([])
+    const [cards, setCards] = useState(false)
     const [count, setCount] = useState(0)// состояние счетчика карты, берем нулевой индекс изначально
-
     useEffect(() => {
         setCards(data)
     }, [])
     let copyCount = count// создаем переменную в которой хванится номер
     function handleClickPrev(item) { // функция которая листакт слайдер назад
-
         if (copyCount > 0) {
-
             copyCount-- // уменьшаем на 1
-        }
-        else {
+        } else {
             copyCount = cards.length
         }
-        setCount(copyCount)
-
+        setCount(copyCount) // записываем в состояние
     }
-
     function handleClickNext() { // функция которая листает слайдер вперед
-
-        if (copyCount < cards.length) {
-
+        if (copyCount < cards.length) { // делаем проверку если индекс больше длинны нашего массива
             copyCount++         //увеличиваем на уедицу чтобы перелеснуть
-        }
-        else {
+        } else {
             copyCount = 0
         }
         setCount(copyCount) // записываем в состояние
     }
-    if (cards.lenght == 0) {
+    if (!cards) {
         return <h1>Loading...</h1>
     }
     return (
         <div className={st.slider}>
-            <button onClick={handleClickPrev} className={st.slider_button}>Предыдущая карточка </button>
+            {console.log(cards)}
+            <button onClick={handleClickPrev} className={st.slider_button}>Предвущая карточка </button>
             <Cards item={cards[count]} />
             <button onClick={handleClickNext} className={st.slider_button}>Следующая карточка </button>
         </div>
